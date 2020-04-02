a7c69203626ba2c47be8c587a7c270affe1cbcd1
Chris Wong <wc38.wong@gmail.com>
2020-04-02 16:27:25 +0800

    Updates

---
+++ precache-manifest.619c7bc041c6703e65c36fcd4f1866b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdaeb7e1744752c3bd08f7762caf472",
+    "url": "/clpsec-game/index.html"
+  },
+  {
+    "revision": "82b5fe2667e8e9d96bb1",
+    "url": "/clpsec-game/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "359a463d32087d3d4b6d",
+    "url": "/clpsec-game/static/css/main.8d38625f.chunk.css"
+  },
+  {
+    "revision": "82b5fe2667e8e9d96bb1",
+    "url": "/clpsec-game/static/js/2.7bd78e95.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/clpsec-game/static/js/2.7bd78e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359a463d32087d3d4b6d",
+    "url": "/clpsec-game/static/js/main.e360d5af.chunk.js"
+  },
+  {
+    "revision": "91c57137cc6b682fdb3d",
+    "url": "/clpsec-game/static/js/runtime-main.687b8d8d.js"
+  }
+]);
