376dd419142c838a4c93e60751b811d30c0da53d
Chris Wong <wc38.wong@gmail.com>
2020-04-03 20:41:46 +0800

    Correct the line chart data

--- src/components/ClickGameLineChart.js
+++ src/components/ClickGameLineChart.js
@@ -10,78 +10,8 @@ import {
 } from 'recharts';
 import moment from 'moment';
 
-// line chart data
-const line_chart_data = [
-  {
-    name: '0',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '0.5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '1',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '1.5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '2',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '2.5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '3',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '3.5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '4',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '4.5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-  {
-    name: '5',
-    orange: 0,
-    blue: 0,
-    black: 0,
-  },
-];
-
 // count click_game by color and elapsed time
-const parseLineChartData = (index, duration, color) => {
+const parseLineChartData = (index, duration, color, line_chart_data) => {
   if (
     // orange or blue
     duration >= parseFloat(line_chart_data[index - 1].name) &&
@@ -98,12 +28,78 @@ const parseLineChartData = (index, duration, color) => {
   }
 };
 
-const renderLegend = (props) => {
-  return <div className="d-flex justify-content-center">Second(s)</div>;
-};
+// calculate all line chart data
+const getAllLineChartData = (click_game) => {
+  // line chart data
+  const line_chart_data = [
+    {
+      name: '0',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '0.5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '1',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '1.5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '2',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '2.5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '3',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '3.5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '4',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '4.5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+    {
+      name: '5',
+      orange: 0,
+      blue: 0,
+      black: 0,
+    },
+  ];
 
-// function component
-function ClickGameLineChart({ click_game }) {
   const first_clicked_at =
     click_game.length > 0 ? click_game[0]['clicked_at'] : null;
   for (let click of click_game) {
@@ -112,14 +108,25 @@ function ClickGameLineChart({ click_game }) {
       .as('seconds');
 
     for (let i = 1; i < line_chart_data.length - 1; i++) {
-      parseLineChartData(i, duration, click.color);
+      parseLineChartData(i, duration, click.color, line_chart_data);
     }
   }
+
+  return line_chart_data;
+};
+
+const renderLegend = (props) => {
+  return <div className="d-flex justify-content-center">Second(s)</div>;
+};
+
+// function component
+function ClickGameLineChart({ click_game }) {
+  const data = getAllLineChartData(click_game);
   return (
     <LineChart
       width={800}
       height={400}
-      data={line_chart_data}
+      data={data}
       margin={{
         top: 5,
         right: 0,
