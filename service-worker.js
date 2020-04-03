cad889172dd9e48be4725efb38dde9783da79214
Chris Wong <wc38.wong@gmail.com>
2020-04-03 10:10:21 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clpsec-game/precache-manifest.6ce0177f58f8534dc04f00290d8403f2.js"
+  "/clpsec-game/precache-manifest.6125f51067963c2bb2be90b7522c6912.js"
 );
 
 self.addEventListener('message', (event) => {
